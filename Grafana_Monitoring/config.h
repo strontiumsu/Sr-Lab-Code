e0b635bcad92d03223999bfa84453fc6dbc0cb76
ejporter <47257372+ejporter@users.noreply.github.com>
2022-04-12 15:55:26 -0700

    Added in additional folders

---
+++ Grafana_Monitoring/config.h
@@ -0,0 +1,11 @@
+#define WIFI_SSID     "wifi_name" // Add wifi name
+#define WIFI_PASSWORD "wifi_password" // Add wifi passowrd
+
+#define ID "room_comfort" // Add unique name for this sensor
+#define INTERVAL 5 //seconds
+
+#define GC_PORT 443
+#define GC_PROM_URL "prometheus-us-central1.grafana.net"
+#define GC_PROM_PATH "/api/prom/push"
+#define GC_PROM_USER ""
+#define GC_PROM_PASS ""
